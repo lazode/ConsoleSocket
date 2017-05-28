cdaa211be825427c960f4c2685cb7fa137ee555e
lanzhuo <lanzhuo_@hotmail.com>
2017-05-28 15:57:28 +0800

    add the socket server code

---
+++ ConsoleSocket-Test/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace ConsoleSocket_Test
+{
+    class Program
+    {
+        public static string localIP = "192.168.243.1";
+        public static int localPort = 86;
+
+        static Socket server = null;
+        static Socket clientSocket = null;
+
+        static List<Socket> clientList = new List<Socket>();
+
+        static byte[] SendBytes = new byte[256];
+        static byte[] RecBytes = new byte[256];
+        static string ClientMSG = null;
+
+        const string fileErrMSG = "Can not read file data now.";
+        const string OKMSG = "Server is OK";
+
+        private static void ListenClientConnect()
+        {
+            while (true)
+            {
+                clientSocket = server.Accept();
+                clientSocket.Send(Encoding.ASCII.GetBytes(OKMSG));
+                clientList.Add(clientSocket);
+                
+                if(clientSocket.Poll(-1, SelectMode.SelectRead) && (clientSocket.Receive(RecBytes) == 0))
+                {
+
+                    clientList.RemoveAt(clientList.Count - 1);
+                }
+            }
+        }
+
+        
+        private static void ResponseClient()
+        {
+            while(true)
+            {
+                if(clientList.Any())
+                {
+                    foreach (Socket item in clientList)
+                    {
+                        
+                        if (item.Poll(10, SelectMode.SelectRead | SelectMode.SelectWrite))
+                        {
+                            if (item.Available > 0)
+                            {
+                                item.Receive(RecBytes, RecBytes.Length, SocketFlags.None);
+                                ClientMSG = Encoding.ASCII.GetString(RecBytes);
+
+                                Console.WriteLine("Message from client:");
+                                Console.WriteLine(ClientMSG);
+                            }
+                             
+                            string fileData = File.ReadAllText(@"C:\Users\lz_home\Desktop\Test.txt", Encoding.ASCII);
+                            //int data = Convert.ToInt32(fileData);
+                            
+                            if (fileData == null)
+                            {
+                                Console.WriteLine("No Data in file now...");
+                                SendBytes = Encoding.ASCII.GetBytes(fileErrMSG);
+                                item.Send(SendBytes, SendBytes.Length, SocketFlags.None);
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("The data in file is:");
+                                Console.WriteLine(fileData);
+
+                                SendBytes = Encoding.ASCII.GetBytes(fileData);
+                                item.Send(SendBytes, SendBytes.Length, SocketFlags.None);
+
+                            }
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No client connecting now...");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+
+                IPAddress localAddr = IPAddress.Parse(localIP);
+                IPEndPoint localEndPoint = new IPEndPoint(localAddr, localPort);
+
+                server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                server.Bind(localEndPoint);
+                server.Listen(10);
+
+                Console.WriteLine("Start listening...");
+
+                Thread ListenThread = new Thread(ListenClientConnect);
+                Thread ResponseThread = new Thread(ResponseClient);
+                ListenThread.Start();
+                ResponseThread.Start();
+                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Please ensure the file existing...");
+
+                Console.WriteLine("Source : " + e.Source);
+                Console.WriteLine("Message : " + e.Message);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException caught!!!");
+                Console.WriteLine("Source : " + e.Source);
+                Console.WriteLine("Message : " + e.Message);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException caught!!!");
+                Console.WriteLine("Source : " + e.Source);
+                Console.WriteLine("Message : " + e.Message);
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("NullReferenceException caught!!!");
+                Console.WriteLine("Source : " + e.Source);
+                Console.WriteLine("Message : " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught!!!");
+                Console.WriteLine("Source : " + e.Source);
+                Console.WriteLine("Message : " + e.Message);
+            }
+
+
+        }
+
+      
+    }
+}
